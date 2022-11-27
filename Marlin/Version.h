ddfaf16ecdc0667d1fdb3f146bcf4a5ecad7fe9e
Michael Haas <haas@computerlinguist.org>
2022-11-27 08:23:04 +0100

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.1.3 MRiscoC"
+#define SHORT_BUILD_VERSION "2.1.3 MRiscoC mhaas baseline"
 
 /**
  * Verbose version identifier which should contain a reference to the location
